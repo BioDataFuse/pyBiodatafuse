0b07185c0d6867b43d68b2f267f6748bc28d374a
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2023-10-18 15:50:59 +0200

    add resourcesdir to constants

--- src/pyBiodatafuse/constants.py
+++ src/pyBiodatafuse/constants.py
@@ -4,9 +4,14 @@
 
 import os
 
+
 HERE = os.path.dirname(os.path.realpath(__file__))  # Src path
 DATA_DIR = os.path.join(HERE, "..", "data")  # Data path
 
 BRIDGEDB_DIR = os.path.join(DATA_DIR, "bridgedb")  # BridgeDb path
 DISGENET_DIR = os.path.join(DATA_DIR, "disgenet")  # DisGeNET path
 COMBINED_DIR = os.path.join(DATA_DIR, "combined_annotated_data")  # Cobimbined data path
+
+RESOURCES_DIR = os.path.join(os.path.dirname(HERE), "..", "resources")  # Resources path
+
+print(BRIDGEDB_DIR)
