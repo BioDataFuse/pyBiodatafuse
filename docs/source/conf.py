2fa9f019cea85520043a1af822c9e218e1c22c8d
YojanaGadiya <yojanagadiya@gmail.com>
2023-10-18 12:30:15 +0200

    Bump version: 0.0.1 → 0.0.2-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = f"{date.today().year}, Tooba Abbassi-Daloii and Yojana Gadiya"
 author = "Tooba Abbassi-Daloii and Yojana Gadiya"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.0.1"
+release = "0.0.2-dev"
 
 # The short X.Y version.
 parsed_version = re.match(
