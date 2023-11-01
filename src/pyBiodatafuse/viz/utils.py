cb6eb952e415da4fef86b471ca7dbf46059e2013
YojanaGadiya <yojanagadiya@gmail.com>
2023-11-01 15:55:09 +0100

    add plot function

--- src/pyBiodatafuse/viz/utils.py
+++ src/pyBiodatafuse/viz/utils.py
@@ -3,13 +3,27 @@
 """Python utils file for plotting functions."""
 
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 
-def plot_pie_chart():
-    """Plot a pie chart."""
-    labels = "Frogs", "Hogs", "Dogs", "Logs"
-    sizes = [15, 30, 45, 10]
+def plot_pie_chart(template_df: pd.DataFrame) -> plt:
+    """Plot a pie chart.
+    
+    :param template_df: A dataframe with two columns: "label" and "value"
+    :returns: A pie chart
+    """
+    plt.figure(figsize=(10, 10))
+    colors = sns.color_palette('pastel')[0:5]
+    plt.pie(template_df["value"], labels=template_df["label"], colors=colors)
+    return plt.show()
 
-    fig, ax = plt.subplots()
-    ax.pie(sizes, labels=labels)
-    plt.show()
+
+def plot_barplot_chart(template_df: pd.DataFrame) -> plt:
+    """Plot a pie chart.
+    
+    :param template_df: A dataframe with two columns: "label" and "value"
+    :returns: A pie chart
+    """
+    sns.barplot(data=template_df, x="label", y="value")
+    return plt.show()
