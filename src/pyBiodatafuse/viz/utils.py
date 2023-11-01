08a18c7d063f6272aeb7594c1cc6a596decba918
YojanaGadiya <yojanagadiya@gmail.com>
2023-11-01 15:04:21 +0100

    update utils

--- src/pyBiodatafuse/viz/utils.py
+++ src/pyBiodatafuse/viz/utils.py
@@ -3,29 +3,13 @@
 """Python utils file for plotting functions."""
 
 import matplotlib.pyplot as plt
-import pandas as pd
-import plotly as px
-import seaborn as sns
 
 
 def plot_pie_chart():
-    pass
+    """Plot a pie chart."""
+    labels = "Frogs", "Hogs", "Dogs", "Logs"
+    sizes = [15, 30, 45, 10]
 
-
-if __name__ == "__main__":
-    from pyBiodatafuse import id_mapper
-
-    chemicals_of_interest = """124211730
-    99997592
-    99995871
-    99990871"""
-    chem_list = chemicals_of_interest.split("\n")
-
-    data_input = pd.DataFrame(chem_list, columns=["identifier"])
-
-    bridgdb_df, bridgdb_metadata = id_mapper.bridgedb_xref(
-        identifiers=data_input["identifier"],
-        input_species="Human",
-        input_datasource="HGNC",
-        output_datasource="All",
-    )
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels)
+    plt.show()
