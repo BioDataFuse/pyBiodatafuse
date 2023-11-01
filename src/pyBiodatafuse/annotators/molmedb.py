b0edf65f75e742b4180a3d96829abbf49dcd8634
DominikMartinat <dominik.martinat@gmail.com>
2023-11-01 16:34:12 +0100

    improved annotator
    Method for transporter inhibitors returns external identifiers and literature DOIs and PMIDs.
    
    Method for transporters inhibited by molecules added. Returns UniProt TrEMBL ids and HGNC ids

--- src/pyBiodatafuse/annotators/molmedb.py
+++ src/pyBiodatafuse/annotators/molmedb.py
@@ -76,6 +76,10 @@ def get_transporter_inhibitor(bridgedb_df: pd.DataFrame):
     # intermediate_df.drop(['pathwayId', 'pathwayLabel', 'geneCount'], axis=1, inplace=True)
 
     intermediate_df.rename(columns={"transporterID": "target"}, inplace=True)
+    intermediate_df["source_doi"] = intermediate_df["source_doi"].map(lambda x: "doi:"+x, na_action="ignore")
+
+    target_columns = list(intermediate_df.columns)
+    target_columns.remove("target")
 
     # Merge the two DataFrames on the target column
     merged_df = collapse_data_sources(
@@ -83,7 +87,7 @@ def get_transporter_inhibitor(bridgedb_df: pd.DataFrame):
         source_namespace="Uniprot-TrEMBL",
         target_df=intermediate_df,
         common_cols=["target"],
-        target_specific_cols=["inhibitor_InChIKey", "inhibitor_SMILES"],
+        target_specific_cols=target_columns,
         col_name="transporter_inhibitor",
     )
 
@@ -98,6 +102,103 @@ def get_transporter_inhibitor(bridgedb_df: pd.DataFrame):
         "datasource": "MolMeDB",
         "query": {
             "size": len(molmedb_transporter_list),
+            "time": time_elapsed,
+            "date": current_date,
+            "url": "https://idsm.elixir-czech.cz/sparql/endpoint/molmedb",
+        },
+    }
+
+    return merged_df, molmedb_metadata
+
+def get_transporter_inhibited(bridgedb_df: pd.DataFrame):
+    """Query MolMeDB for transporters inhibited by molecule.
+
+    :param bridgedb_df: BridgeDb output for creating the list of gene ids to query
+    :returns: a DataFrame containing the MolMeDB output and dictionary of the MolMeDB metadata.
+    """
+    # Record the start time
+    start_time = datetime.datetime.now()
+
+    data_df = get_identifier_of_interest(bridgedb_df, "InChIKey")
+    inhibitor_list_str = data_df["target"].tolist()
+
+    inhibitor_list_str = list(set(inhibitor_list_str))
+
+    query_inhibitor_list = []
+
+    if len(inhibitor_list_str) > 25:
+        for i in range(0, len(inhibitor_list_str), 25):
+            tmp_list = inhibitor_list_str[i : i + 25]
+            query_inhibitor_list.append(" ".join(f'"{g}"' for g in tmp_list))
+
+    else:
+        query_inhibitor_list.append(" ".join(f'"{g}"' for g in inhibitor_list_str))
+
+    with open(
+        os.path.dirname(__file__) + "/queries/molmedb-transporters-inhibited-by-molecule.rq", "r"
+    ) as fin:
+        sparql_query = fin.read()
+
+    sparql = SPARQLWrapper("https://idsm.elixir-czech.cz/sparql/endpoint/molmedb")
+    sparql.setReturnFormat(JSON)
+    sparql.setOnlyConneg(True)
+
+    query_count = 0
+
+    results_df_list = list()
+
+    for inhibitor_list_str in query_inhibitor_list:
+        query_count += 1
+
+        sparql_query_template = Template(sparql_query)
+        substit_dict = dict(inhibitor_list=inhibitor_list_str)
+        sparql_query_template_sub = sparql_query_template.substitute(substit_dict)
+
+        sparql.setQuery(sparql_query_template_sub)
+
+        res = sparql.queryAndConvert()
+
+        df = pd.DataFrame(res["results"]["bindings"])
+        df = df.applymap(lambda x: x["value"], na_action="ignore")
+
+        results_df_list.append(df)
+
+    # Record the end time
+    end_time = datetime.datetime.now()
+
+    # Organize the annotation results as an array of dictionaries
+    intermediate_df = pd.concat(results_df_list)
+
+    # intermediate_df = intermediate_df.groupby('geneId').apply(lambda x: x.to_dict(orient='r')).rename('WikiPathways')
+    # intermediate_df.drop(['pathwayId', 'pathwayLabel', 'geneCount'], axis=1, inplace=True)
+
+    intermediate_df.rename(columns={"inhibitorInChIKey": "target"}, inplace=True)
+    intermediate_df["source_doi"] = intermediate_df["source_doi"].map(lambda x: "doi:"+x, na_action="ignore")
+    
+    target_columns = list(intermediate_df.columns)
+    target_columns.remove("target")
+
+    # Merge the two DataFrames on the target column
+    merged_df = collapse_data_sources(
+        data_df=data_df,
+        source_namespace="InChIKey",
+        target_df=intermediate_df,
+        common_cols=["target"],
+        target_specific_cols=target_columns,
+        col_name="transporter_inhibited",
+    )
+
+    # Metdata details
+    # Get the current date and time
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Calculate the time elapsed
+    time_elapsed = str(end_time - start_time)
+
+    # Add the datasource, query, query time, and the date to metadata
+    molmedb_metadata = {
+        "datasource": "MolMeDB",
+        "query": {
+            "size": len(inhibitor_list_str),
             "time": time_elapsed,
             "date": current_date,
             "url": "https://idsm.elixir-czech.cz/sparql/endpoint/molmedb",
