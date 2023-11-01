2fd9651feb8e019d0ab9a938585dd6553ec8d96a
DominikMartinat <dominik.martinat@gmail.com>
2023-11-01 09:44:53 +0100

    transporter inhibitors

---
+++ src/pyBiodatafuse/annotators/molmedb.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Python file for queriying MolMeDB SPARQL endpoint (https://idsm.elixir-czech.cz/sparql/endpoint/molmedb."""
+
+import datetime
+import os
+from string import Template
+
+import pandas as pd
+from SPARQLWrapper import JSON, SPARQLWrapper
+
+from pyBiodatafuse.utils import collapse_data_sources, get_identifier_of_interest
+
+
+def get_transporter_inhibitor(bridgedb_df: pd.DataFrame):
+    """Query MolMeDB for inhibitors of transporters encoded by genes.
+
+    :param bridgedb_df: BridgeDb output for creating the list of gene ids to query
+    :returns: a DataFrame containing the MolMeDB output and dictionary of the MolMeDB metadata.
+    """
+    # Record the start time
+    start_time = datetime.datetime.now()
+
+    data_df = get_identifier_of_interest(bridgedb_df, "Uniprot-TrEMBL")
+    molmedb_transporter_list = data_df["target"].tolist()
+
+    molmedb_transporter_list = list(set(molmedb_transporter_list))
+
+    query_transporter_list = []
+
+    if len(molmedb_transporter_list) > 25:
+        for i in range(0, len(molmedb_transporter_list), 25):
+            tmp_list = molmedb_transporter_list[i : i + 25]
+            query_transporter_list.append(" ".join(f'"{g}"' for g in tmp_list))
+
+    else:
+        query_transporter_list.append(" ".join(f'"{g}"' for g in molmedb_transporter_list))
+
+    with open(
+        os.path.dirname(__file__) + "/queries/molmedb-transporters-inhibitors.rq", "r"
+    ) as fin:
+        sparql_query = fin.read()
+
+    sparql = SPARQLWrapper("https://idsm.elixir-czech.cz/sparql/endpoint/molmedb")
+    sparql.setReturnFormat(JSON)
+    sparql.setOnlyConneg(True)
+
+    query_count = 0
+
+    results_df_list = list()
+
+    for transporter_list_str in query_transporter_list:
+        query_count += 1
+
+        sparql_query_template = Template(sparql_query)
+        substit_dict = dict(transporter_list=transporter_list_str)
+        sparql_query_template_sub = sparql_query_template.substitute(substit_dict)
+
+        sparql.setQuery(sparql_query_template_sub)
+
+        res = sparql.queryAndConvert()
+
+        df = pd.DataFrame(res["results"]["bindings"])
+        df = df.applymap(lambda x: x["value"], na_action="ignore")
+
+        results_df_list.append(df)
+
+    # Record the end time
+    end_time = datetime.datetime.now()
+
+    # Organize the annotation results as an array of dictionaries
+    intermediate_df = pd.concat(results_df_list)
+
+    # intermediate_df = intermediate_df.groupby('geneId').apply(lambda x: x.to_dict(orient='r')).rename('WikiPathways')
+    # intermediate_df.drop(['pathwayId', 'pathwayLabel', 'geneCount'], axis=1, inplace=True)
+
+    intermediate_df.rename(columns={"transporterID": "target"}, inplace=True)
+
+    # Merge the two DataFrames on the target column
+    merged_df = collapse_data_sources(
+        data_df=data_df,
+        source_namespace="Uniprot-TrEMBL",
+        target_df=intermediate_df,
+        common_cols=["target"],
+        target_specific_cols=["inhibitor_InChIKey", "inhibitor_SMILES"],
+        col_name="transporter_inhibitor",
+    )
+
+    # Metdata details
+    # Get the current date and time
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Calculate the time elapsed
+    time_elapsed = str(end_time - start_time)
+
+    # Add the datasource, query, query time, and the date to metadata
+    molmedb_metadata = {
+        "datasource": "MolMeDB",
+        "query": {
+            "size": len(molmedb_transporter_list),
+            "time": time_elapsed,
+            "date": current_date,
+            "url": "https://idsm.elixir-czech.cz/sparql/endpoint/molmedb",
+        },
+    }
+
+    return merged_df, molmedb_metadata
