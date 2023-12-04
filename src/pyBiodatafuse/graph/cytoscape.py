37bcf089d61263c2e67304a4856a0c87812d6712
Ammar Ammar <ammar257ammar@gmail.com>
2023-12-04 10:15:31 +0100

    Add function to load the graph into live Cytoscape instance

--- src/pyBiodatafuse/graph/cytoscape.py
+++ src/pyBiodatafuse/graph/cytoscape.py
@@ -5,6 +5,8 @@
 import json
 
 import networkx as nx
+import py4cytoscape as p4c
+
 
 def _replace_graph_attrs_for_cytoscape(g: nx.MultiDiGraph):
     """Adapt the node and edge attributes keys to the cytoscape json structure.
@@ -41,6 +43,52 @@ def convert_graph_to_cytoscapejs_json(g: nx.MultiDiGraph):
     cytoscape_graph = nx.cytoscape_data(adj_g)
 
     return cytoscape_graph
+
+
+def load_graph_into_cytoscape(g: nx.MultiDiGraph, network_name: str):
+    """Load the obtained graph into a running instance of Cytoscape.
+
+    :param g: input NetworkX graph object.
+    :param network_name: Network name to appear in Cytoscape.
+    """
+    adj_g = _replace_graph_attrs_for_cytoscape(g)
+
+    # Define the visual style as a dictionary
+    default = {
+        "title": "BioDataFuse_style",
+        "defaults": [
+            {"visualProperty": "NODE_FILL_COLOR", "value": "#FF0000"},
+            {"visualProperty": "EDGE_COLOR", "value": "#000000"},
+        ],
+        "mappings": [],
+    }
+
+    # Create the network in Cytoscape
+    p4c.networks.create_network_from_networkx(
+        adj_g,
+        title=network_name,
+        collection="BioDataFuse",
+    )
+
+    # Apply the visual style
+    p4c.styles.create_visual_style(default)
+
+    # Define node shape and color mapping
+    column = "node_type"
+    values = [
+        "gene",
+        "disease",
+        "gene ontology",
+        "reactome pathways",
+        "drug interactions",
+    ]
+    shapes = ["DIAMOND", "RECTANGLE", "OCTAGON", "HEXAGON", "ELLIPSE"]
+    colors = ["#AAFF88", "#B0C4DE", "pink", "yellow", "red"]
+
+    # Apply node shape and color mappings
+    p4c.set_node_color_mapping(column, values, colors, mapping_type="d", style_name="default")
+
+    p4c.set_node_shape_mapping(column, values, shapes, style_name="default")
 
 
 def save_cytoscape_json_to_file(cytoscape_graph: dict, output_path: str):
