4817c9aee7b62e0959c09709642920fbc97f76af
Ammar Ammar <ammar257ammar@gmail.com>
2024-02-09 11:29:52 +0100

    Fix WikiPathways test documentation for Flake

--- tests/annotators/test_wikipathways.py
+++ tests/annotators/test_wikipathways.py
@@ -1,36 +1,60 @@
-from pyBiodatafuse.annotators.wikipathways import get_version_wikipathways, get_gene_wikipathway
-import pytest
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for the WikiPathways annotator."""
+
 import pandas as pd
-import numpy as np
+import pytest
 from numpy import nan
 
+from pyBiodatafuse.annotators.wikipathways import get_gene_wikipathway, get_version_wikipathways
+
+
 def test_wikipathways_version():
+    """Test the get_version_wikipathways."""
+    obtained_version = get_version_wikipathways()
 
-    obtained_version = get_version_wikipathways();
+    expected_version = {"wikipathways_version": "WikiPathways RDF 20231210"}
 
-    expected_version = {"wikipathways_version": 'WikiPathways RDF 20231210'}
-
-    assert obtained_version ==  expected_version
+    assert obtained_version == expected_version
 
 
 def test_get_gene_wikipathway(bridgedb_dataframe):
-
+    """Test the get_gene_wikipathway."""
     obtained_data, metadata = get_gene_wikipathway(bridgedb_dataframe)
 
-    expected_data = pd.Series([[{'pathwayId': 'WP5153', 'pathwayLabel': 'N-glycan biosynthesis', 'pathwayGeneCount': '57'}],
-                               [{'pathwayId': 'WP5153', 'pathwayLabel': 'N-glycan biosynthesis', 'pathwayGeneCount': '57'}],
-                               [{'pathwayId': nan, 'pathwayLabel': nan, 'pathwayGeneCount': nan}]])
-    expected_data.name = 'WikiPathways'
+    expected_data = pd.Series(
+        [
+            [
+                {
+                    "pathwayId": "WP5153",
+                    "pathwayLabel": "N-glycan biosynthesis",
+                    "pathwayGeneCount": "57",
+                }
+            ],
+            [
+                {
+                    "pathwayId": "WP5153",
+                    "pathwayLabel": "N-glycan biosynthesis",
+                    "pathwayGeneCount": "57",
+                }
+            ],
+            [{"pathwayId": nan, "pathwayLabel": nan, "pathwayGeneCount": nan}],
+        ]
+    )
+    expected_data.name = "WikiPathways"
 
     pd.testing.assert_series_equal(obtained_data["WikiPathways"], expected_data)
 
 
 @pytest.fixture(scope="module")
 def bridgedb_dataframe():
-    return pd.DataFrame({
-        'identifier': ['ALG14', 'ALG2', 'CHRNA1'],
-        'identifier.source': ['HGNC', 'HGNC', 'HGNC'],
-        'target': ['199857', '85365', '1134'],
-        'target.source': ['NCBI Gene', 'NCBI Gene', 'NCBI Gene']
-    })
-
+    """Reusable sample Pandas DataFrame to be used as input for the tests."""
+    return pd.DataFrame(
+        {
+            "identifier": ["ALG14", "ALG2", "CHRNA1"],
+            "identifier.source": ["HGNC", "HGNC", "HGNC"],
+            "target": ["199857", "85365", "1134"],
+            "target.source": ["NCBI Gene", "NCBI Gene", "NCBI Gene"],
+        }
+    )
