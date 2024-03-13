921e943fab19f7d231c17e7f7557529d9c1fc03a
DominikMartinat <71714049+DominikMartinat@users.noreply.github.com>
2024-03-13 18:57:12 +0100

    Pubchem annotator (#98)
    * pubchem annotator
    
    * pubchem annotator
    
    * test fix
    
    * assay type and outcome pruning

---
+++ src/pyBiodatafuse/annotators/pubchem.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Python file for queriying PubChem SPARQL endpoint at https://idsm.elixir-czech.cz/sparql/endpoint/idsm."""
+
+import datetime
+import os
+import warnings
+from string import Template
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+from SPARQLWrapper import JSON, SPARQLWrapper
+
+from pyBiodatafuse.utils import collapse_data_sources, get_identifier_of_interest
+
+
+def test_idsm_endpoint(endpoint: str) -> bool:
+    """Test the availability of the IDSM endpoint.
+
+    :param endpoint: IDSM endpoint ("https://idsm.elixir-czech.cz/sparql/endpoint/idsm")
+    :returns: True if the endpoint is available, False otherwise.
+    """
+    query_string = """SELECT * WHERE {
+        <http://rdf.ncbi.nlm.nih.gov/pubchem/taxonomy> ?p ?o
+        }
+        LIMIT 1
+        """
+    sparql = SPARQLWrapper(endpoint)
+    sparql.setOnlyConneg(True)
+    sparql.setQuery(query_string)
+    try:
+        sparql.query()
+        return True
+    except BaseException:
+        return False
+
+
+def get_protein_molecule_screened(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame, dict]:
+    """Query PubChem for molecules screened on proteins as targets.
+
+    :param bridgedb_df: BridgeDb output for creating the list of gene ids to query.
+    :returns: a DataFrame containing the PubChem output and dictionary of the PubChem metadata.
+    """
+    # Check if the IDSM endpoint is available
+    endpoint = "https://idsm.elixir-czech.cz/sparql/endpoint/idsm"
+    api_available = test_idsm_endpoint(endpoint=endpoint)
+    if not api_available:
+        warnings.warn("PubChem endpoint is not available. Unable to retrieve data.", stacklevel=2)
+        return pd.DataFrame(), {}
+
+    # Record the start time
+    start_time = datetime.datetime.now()
+
+    data_df = get_identifier_of_interest(bridgedb_df, "Uniprot-TrEMBL")
+    protein_list_str = data_df["target"].tolist()
+    for i in range(len(protein_list_str)):
+        protein_list_str[i] = "<http://purl.uniprot.org/uniprot/" + protein_list_str[i] + ">"
+
+    protein_list_str = list(set(protein_list_str))
+
+    query_protein_list = []
+
+    if len(protein_list_str) > 25:
+        for i in range(0, len(protein_list_str), 25):
+            tmp_list = protein_list_str[i : i + 25]
+            query_protein_list.append(" ".join(f"{g}" for g in tmp_list))
+
+    else:
+        query_protein_list.append(" ".join(f"{g}" for g in protein_list_str))
+
+    with open(
+        os.path.dirname(__file__) + "/queries/pubchem-proteins-screend-molecule.rq", "r"
+    ) as fin:
+        sparql_query = fin.read()
+
+    sparql = SPARQLWrapper("https://idsm.elixir-czech.cz/sparql/endpoint/idsm")
+    sparql.setReturnFormat(JSON)
+    sparql.setOnlyConneg(True)
+
+    query_count = 0
+
+    results_df_list = list()
+
+    for protein_list_str in query_protein_list:
+        query_count += 1
+
+        sparql_query_template = Template(sparql_query)
+        substit_dict = dict(protein_list=protein_list_str)
+        sparql_query_template_sub = sparql_query_template.substitute(substit_dict)
+
+        sparql.setQuery(sparql_query_template_sub)
+        res = sparql.queryAndConvert()
+
+        df = pd.DataFrame(res["results"]["bindings"])
+        df = df.applymap(lambda x: x["value"], na_action="ignore")
+
+        results_df_list.append(df)
+
+    # Record the end time
+    end_time = datetime.datetime.now()
+
+    # Organize the annotation results as an array of dictionaries
+    intermediate_df = pd.concat(results_df_list)
+
+    intermediate_df.rename(columns={"upProt": "target"}, inplace=True)
+
+    endpoint_types = {
+        "http://www.bioassayontology.org/bao#BAO_0000034": "Kd",
+        "http://www.bioassayontology.org/bao#BAO_0000186": "AC50",
+        "http://www.bioassayontology.org/bao#BAO_0000187": "CC50",
+        "http://www.bioassayontology.org/bao#BAO_0000188": "EC50",
+        "http://www.bioassayontology.org/bao#BAO_0000190": "IC50",
+        "http://www.bioassayontology.org/bao#BAO_0000192": "Ki",
+        "http://www.bioassayontology.org/bao#BAO_0002146": "MIC",
+    }
+
+    if not intermediate_df.empty:
+        # drop multitarget assays
+        intermediate_df["target_count"] = intermediate_df["target_count"].map(lambda x: int(x))
+        intermediate_df = intermediate_df.drop(
+            intermediate_df[intermediate_df["target_count"] > 1].index
+        )
+        intermediate_df = intermediate_df.drop(columns=["target_count"])
+        # identifiers to values
+        intermediate_df["target"] = intermediate_df["target"].map(lambda x: x[32:])
+        intermediate_df["outcome"] = intermediate_df["outcome"].map(lambda x: x[47:])
+        intermediate_df["compound_cid"] = intermediate_df["compound_cid"].map(lambda x: x[48:])
+        intermediate_df["assay_type"] = intermediate_df["assay_type"].map(
+            lambda x: endpoint_types[x]
+        )
+        target_columns = list(intermediate_df.columns)
+        target_columns.remove("target")
+    else:
+        target_columns = list(intermediate_df.columns)
+
+    # Merge the two DataFrames on the target column
+    merged_df = collapse_data_sources(
+        data_df=data_df,
+        source_namespace="Uniprot-TrEMBL",
+        target_df=intermediate_df,
+        common_cols=["target"],
+        target_specific_cols=target_columns,
+        col_name="compounds_screened",
+    )
+
+    # if mappings exist but SPARQL returns empty response
+    if (not merged_df.empty) and merged_df["compounds_screened"][0] is None:
+        merged_df.drop_duplicates(subset=["identifier", "compounds_screened"], inplace=True)
+    elif not merged_df.empty:
+        res_keys = merged_df["compounds_screened"][0][0].keys()
+        # remove duplicate identifier and response row
+        merged_df["compounds_screened"] = merged_df["compounds_screened"].map(
+            lambda x: tuple(frozenset(d.items()) for d in x), na_action="ignore"
+        )
+        merged_df.drop_duplicates(subset=["identifier", "compounds_screened"], inplace=True)
+        merged_df["compounds_screened"] = merged_df["compounds_screened"].map(
+            lambda res_tup: list(dict((x, y) for x, y in res) for res in res_tup),
+            na_action="ignore",
+        )
+
+        # drop rows with duplicate identifiers with empty response
+        identifiers = merged_df["identifier"].unique()
+        for identifier in identifiers:
+            if merged_df.loc[merged_df["identifier"] == identifier].shape[0] > 1:
+                mask = merged_df["compounds_screened"].apply(
+                    lambda lst: all(
+                        [
+                            all([isinstance(val, float) and np.isnan(val) for val in dct.values()])
+                            for dct in lst
+                        ]
+                    )
+                )
+                mask2 = merged_df["identifier"].apply(lambda x, id=identifier: x == id)
+                merged_df.drop(merged_df[mask & mask2].index, inplace=True)
+
+        # set default order to response dictionaries to keep output consistency
+        merged_df["compounds_screened"] = merged_df["compounds_screened"].apply(
+            lambda res: list(dict((k, r[k]) for k in res_keys) for r in res)
+        )
+        # set numerical identifiers to int to kepp output consistency
+        merged_df["compounds_screened"] = merged_df["compounds_screened"].apply(
+            lambda res: int_response_value_types(res, ["compound_cid"])
+        )
+    merged_df.reset_index(drop=True, inplace=True)
+
+    # Metdata details
+    # Get the current date and time
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Calculate the time elapsed
+    time_elapsed = str(end_time - start_time)
+
+    # Add the datasource, query, query time, and the date to metadata
+    molmedb_metadata = {
+        "datasource": "PubChem",
+        "query": {
+            "size": len(protein_list_str),
+            "time": time_elapsed,
+            "date": current_date,
+            "url": "https://idsm.elixir-czech.cz/sparql/endpoint/idsm",
+        },
+    }
+
+    return merged_df, molmedb_metadata
+
+
+def int_response_value_types(resp_list: list, key_list: list):
+    """Change values in response dictionaries to int to stay consistent with other Annotators.
+
+    :param: resp_list: list of response dictionaries.
+    :param: key_list: list of keys to change to int.
+    :returns: resp_list with int values in response dictionaries on keys in key_list.
+    """
+    for r in resp_list:
+        for k in key_list:
+            try:
+                r[k] = int(r[k])
+            except ValueError:
+                continue
+    return resp_list
---
+++ tests/annotators/test_pubchem.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for the WikiPathways annotator."""
+
+from unittest.mock import patch
+
+import pandas as pd
+import pytest
+from numpy import nan
+
+from pyBiodatafuse.annotators.pubchem import get_protein_molecule_screened
+
+
+@patch("pyBiodatafuse.annotators.pubchem.SPARQLWrapper.queryAndConvert")
+def test_get_protein_molecule_screened(mock_sparql_request, bridgedb_dataframe):
+    """Test the get_protein_molecule_screened."""
+    mock_sparql_request.side_effect = [
+        {
+            "head": {
+                "vars": [
+                    "upProt",
+                    "assay_type",
+                    "outcome",
+                    "compound_cid",
+                    "ref_cit",
+                    "compound_name",
+                    "SMILES",
+                    "InChI",
+                ]
+            },
+            "results": {
+                "bindings": [
+                    {
+                        "upProt": {
+                            "type": "uri",
+                            "value": "http://purl.uniprot.org/uniprot/P46098",
+                        },
+                        "assay_type": {
+                            "type": "uri",
+                            "value": "http://www.bioassayontology.org/bao#BAO_0000192",
+                        },
+                        "outcome": {
+                            "type": "uri",
+                            "value": "http://rdf.ncbi.nlm.nih.gov/pubchem/vocabulary#active",
+                        },
+                        "compound_cid": {
+                            "type": "uri",
+                            "value": "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID9911844",
+                        },
+                        "ref_cit": {
+                            "type": "literal",
+                            "value": "Kikuchi C, Suzuki H, Hiranuma T, Koyama M.",
+                            "datatype": "http://www.w3.org/2001/XMLSchema#string",
+                        },
+                        "compound_name": {
+                            "type": "literal",
+                            "value": "DR-4485 free base",
+                            "datatype": "http://www.w3.org/2001/XMLSchema#string",
+                        },
+                        "SMILES": {
+                            "type": "literal",
+                            "value": "C1CC2=C(C=CC3=C2C(C1)(C(=O)N3)CCCCN4CCC(=CC4)C5=CC=C(C=C5)Cl)Cl",
+                            "xml:lang": "en",
+                        },
+                        "InChI": {
+                            "type": "literal",
+                            "value": "InChI=1S/C26H28Cl2N2O/c27",
+                            "xml:lang": "en",
+                        },
+                        "target_count": {
+                            "type": "literal",
+                            "value": "1",
+                            "datatype": "http://www.w3.org/2001/XMLSchema#integer",
+                        },
+                    },
+                    {
+                        "upProt": {
+                            "type": "uri",
+                            "value": "http://purl.uniprot.org/uniprot/P00533",
+                        },
+                        "assay_type": {
+                            "type": "uri",
+                            "value": "http://www.bioassayontology.org/bao#BAO_0000186",
+                        },
+                        "outcome": {
+                            "type": "uri",
+                            "value": "http://rdf.ncbi.nlm.nih.gov/pubchem/vocabulary#inconclusive",
+                        },
+                        "compound_cid": {
+                            "type": "uri",
+                            "value": "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID5151543",
+                        },
+                        "compound_name": {
+                            "type": "literal",
+                            "value": "1-(4-Methylpiperazin-1-yl)anthra-9,10-quinone",
+                            "datatype": "http://www.w3.org/2001/XMLSchema#string",
+                        },
+                        "SMILES": {
+                            "type": "literal",
+                            "value": "CN1CCN(CC1)C2=CC=CC3=C2C(=O)C4=CC=CC=C4C3=O",
+                            "xml:lang": "en",
+                        },
+                        "InChI": {
+                            "type": "literal",
+                            "value": "InChI=1S/C19H18N2O2/c1",
+                            "xml:lang": "en",
+                        },
+                        "target_count": {
+                            "type": "literal",
+                            "value": "1",
+                            "datatype": "http://www.w3.org/2001/XMLSchema#integer",
+                        },
+                    },
+                    {
+                        "upProt": {
+                            "type": "uri",
+                            "value": "http://purl.uniprot.org/uniprot/P00533",
+                        },
+                        "assay_type": {
+                            "type": "uri",
+                            "value": "http://www.bioassayontology.org/bao#BAO_0000186",
+                        },
+                        "outcome": {
+                            "type": "uri",
+                            "value": "http://rdf.ncbi.nlm.nih.gov/pubchem/vocabulary#inconclusive",
+                        },
+                        "compound_cid": {
+                            "type": "uri",
+                            "value": "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID6706",
+                        },
+                        "compound_name": {
+                            "type": "literal",
+                            "value": "1-(Methylamino)anthraquinone",
+                            "datatype": "http://www.w3.org/2001/XMLSchema#string",
+                        },
+                        "SMILES": {
+                            "type": "literal",
+                            "value": "CNC1=CC=CC2=C1C(=O)C3=CC=CC=C3C2=O",
+                            "xml:lang": "en",
+                        },
+                        "InChI": {
+                            "type": "literal",
+                            "value": "InChI=1S/C15H11NO2/c1",
+                            "xml:lang": "en",
+                        },
+                        "target_count": {
+                            "type": "literal",
+                            "value": "1",
+                            "datatype": "http://www.w3.org/2001/XMLSchema#integer",
+                        },
+                    },
+                ]
+            },
+        }
+    ]
+
+    obtained_data, metadata = get_protein_molecule_screened(bridgedb_dataframe)
+
+    expected_data = pd.Series(
+        [
+            [
+                {
+                    "assay_type": "AC50",
+                    "outcome": "inconclusive",
+                    "compound_cid": 5151543,
+                    "compound_name": "1-(4-Methylpiperazin-1-yl)anthra-9,10-quinone",
+                    "SMILES": "CN1CCN(CC1)C2=CC=CC3=C2C(=O)C4=CC=CC=C4C3=O",
+                    "InChI": "InChI=1S/C19H18N2O2/c1",
+                    "ref_cit": nan,
+                },
+                {
+                    "assay_type": "AC50",
+                    "outcome": "inconclusive",
+                    "compound_cid": 6706,
+                    "compound_name": "1-(Methylamino)anthraquinone",
+                    "SMILES": "CNC1=CC=CC2=C1C(=O)C3=CC=CC=C3C2=O",
+                    "InChI": "InChI=1S/C15H11NO2/c1",
+                    "ref_cit": nan,
+                },
+            ],
+            [
+                {
+                    "assay_type": "Ki",
+                    "outcome": "active",
+                    "compound_cid": 9911844,
+                    "compound_name": "DR-4485 free base",
+                    "SMILES": "C1CC2=C(C=CC3=C2C(C1)(C(=O)N3)CCCCN4CCC(=CC4)C5=CC=C(C=C5)Cl)Cl",
+                    "InChI": "InChI=1S/C26H28Cl2N2O/c27",
+                    "ref_cit": "Kikuchi C, Suzuki H, Hiranuma T, Koyama M.",
+                }
+            ],
+        ]
+    )
+    expected_data.name = "compounds_screened"
+
+    pd.testing.assert_series_equal(obtained_data["compounds_screened"], expected_data)
+
+
+@pytest.fixture(scope="module")
+def bridgedb_dataframe():
+    """Reusable sample Pandas DataFrame to be used as input for the tests."""
+    return pd.DataFrame(
+        {
+            "identifier": ["EGFR", "EGFR", "EGFR", "HTR3A", "HTR3A"],
+            "identifier.source": ["HGNC", "HGNC", "HGNC", "HGNC", "HGNC"],
+            "target": ["C9JYS6", "P00533", "Q504U8", "A0A0B4J205", "P46098"],
+            "target.source": [
+                "Uniprot-TrEMBL",
+                "Uniprot-TrEMBL",
+                "Uniprot-TrEMBL",
+                "Uniprot-TrEMBL",
+                "Uniprot-TrEMBL",
+            ],
+        }
+    )
