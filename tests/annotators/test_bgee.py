6cd9e213ccd2222dee1dd9cbbbf3e5b9d27e635b
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2024-03-15 11:25:05 +0100

    Update test_bgee.py

--- tests/annotators/test_bgee.py
+++ tests/annotators/test_bgee.py
@@ -5,10 +5,9 @@
 
 import json
 import os
-from unittest.mock import patch
-
 import pandas as pd
 import pytest
+from unittest.mock import patch
 
 from pyBiodatafuse.annotators.bgee import get_gene_expression, get_version_bgee
 
