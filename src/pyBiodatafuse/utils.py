e42398fe18cf31d5f48ce1e812ae94c067d8b430
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2024-08-15 16:43:51 +0200

    add function to annotate for a list of datasource

--- src/pyBiodatafuse/utils.py
+++ src/pyBiodatafuse/utils.py
@@ -6,8 +6,10 @@ import warnings
 from typing import List
 
 import pandas as pd
+from collections import defaultdict
 
 from pyBiodatafuse.id_mapper import read_resource_files
+from pyBiodatafuse.annotators import bgee, disgenet, minerva, molmedb, opentargets, pubchem, stringdb, wikipathways
 
 
 def get_identifier_of_interest(bridgedb_df: pd.DataFrame, db_source: str) -> pd.DataFrame:
@@ -151,3 +153,44 @@ def check_columns_against_constants(
                         f"All values in column '{col}' do not start with '{starts_with}'.",
                         stacklevel=2,
                     )
+
+
+def process_selected_sources(
+    bridgedb_df: pd.DataFrame, selected_sources_list: list
+) -> pd.DataFrame:
+    """query the selected databases and convert the output to a dataframe.
+
+    @param bridgedb_df: BridgeDb output for creating the list of gene ids to query
+    @param selected_sources_list: list of selected databases
+    """
+    # Initialize variables
+    combined_data = pd.DataFrame()
+    combined_metadata = defaultdict(lambda: defaultdict(str))
+    # Dictionary to map the datasource names to their corresponding functions
+    data_source_functions = {
+        ## TODO: "bgee": bgee.get_gene_expression,
+        "disgenet": disgenet.get_gene_disease,
+        ## TODO: "minerva": minera.get,
+        ## TODO: "molmedb": molmedb.get_mol_gene_inhibitor,
+        ## TODO: "pubchem"
+        "opentarget.gene_ontology": opentargets.get_gene_go_process,
+        "opentarget.reactome": opentargets.get_gene_reactome_pathways,
+        "opentarget.drug_interactions": opentargets.get_gene_compound_interactions,
+        "opentarget.disease_associations": opentargets.get_gene_disease_interactions,
+        "string": stringdb.get_ppi,
+        "wikipathways": wikipathways.get_gene_wikipathway
+        ## TODO: "wikidata"
+    }
+    warnings = []  # Initialize empty list for warnings
+
+    for source in selected_sources_list:
+        if source in data_source_functions:
+            print(source)
+            tmp_data, tmp_metadata = data_source_functions[source](bridgedb_df)
+            combined_metadata[source] = tmp_metadata
+            if tmp_data.empty:
+                warnings.append(f"No annotation available for {source}")
+            if not tmp_data.empty:
+                combined_data = combine_sources([combined_data, tmp_data])
+
+    return combined_data, combined_metadata
