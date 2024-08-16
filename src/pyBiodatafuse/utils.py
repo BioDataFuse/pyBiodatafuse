696cb00e4a69f03de767a3c5e1f2c34af74ff762
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2024-08-16 10:38:45 +0200

    tox fix

--- src/pyBiodatafuse/utils.py
+++ src/pyBiodatafuse/utils.py
@@ -167,28 +167,29 @@ def check_columns_against_constants(
 def process_selected_sources(
     bridgedb_df: pd.DataFrame, selected_sources_list: list
 ) -> pd.DataFrame:
-    """query the selected databases and convert the output to a dataframe.
+    """Query the selected databases and convert the output to a dataframe.
 
-    @param bridgedb_df: BridgeDb output for creating the list of gene ids to query
-    @param selected_sources_list: list of selected databases
+    :param bridgedb_df: BridgeDb output for creating the list of gene ids to query.
+    :param selected_sources_list: list of selected databases
+    :returns: a DataFrame containing the combined output and dictionary of the metadata.
     """
     # Initialize variables
     combined_data = pd.DataFrame()
     combined_metadata = defaultdict(lambda: defaultdict(str))
     # Dictionary to map the datasource names to their corresponding functions
     data_source_functions = {
-        ## TODO: "bgee": bgee.get_gene_expression,
+        # TODO: "bgee": bgee.get_gene_expression,
         "disgenet": disgenet.get_gene_disease,
-        ## TODO: "minerva": minera.get,
-        ## TODO: "molmedb": molmedb.get_mol_gene_inhibitor,
-        ## TODO: "pubchem"
+        # TODO: "minerva": minera.get,
+        # TODO: "molmedb": molmedb.get_mol_gene_inhibitor,
+        # TODO: "pubchem"
         "opentarget.gene_ontology": opentargets.get_gene_go_process,
         "opentarget.reactome": opentargets.get_gene_reactome_pathways,
         "opentarget.drug_interactions": opentargets.get_gene_compound_interactions,
         "opentarget.disease_associations": opentargets.get_gene_disease_interactions,
         "string": stringdb.get_ppi,
         "wikipathways": wikipathways.get_gene_wikipathway
-        ## TODO: "wikidata"
+        # TODO: "wikidata"
     }
     warnings = []  # Initialize empty list for warnings
 
