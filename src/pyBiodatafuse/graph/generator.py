1dd02cbf1487715f92216ba51587ad40b4749437
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2024-09-18 14:44:14 +0200

    tox

--- src/pyBiodatafuse/graph/generator.py
+++ src/pyBiodatafuse/graph/generator.py
@@ -233,7 +233,6 @@ def add_literature_gene_disease_subgraph(g, gene_node_label, annot_list):
             annot_node_attrs["name"] = annot["disease_name"]
             annot_node_attrs["id"] = annot["id"]
             annot_node_attrs["UMLS"] = annot["id"]
-            
 
             g.add_node(annot_node_label, attr_dict=annot_node_attrs)
 
