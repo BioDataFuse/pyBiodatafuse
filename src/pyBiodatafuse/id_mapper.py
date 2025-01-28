fcdd85ff9b279c1b0bdee1d739abc035b66a287e
Javier Millán Acosta <83466805+jmillanacosta@users.noreply.github.com>
2025-01-28 12:41:25 +0100

    Restore PubChem output_datasource
    Additionally, why not retrieve the list from datasources.tsv?

--- src/pyBiodatafuse/id_mapper.py
+++ src/pyBiodatafuse/id_mapper.py
@@ -115,6 +115,7 @@ def bridgedb_xref(
             "Ensembl",
             "HGNC Accession Number",
             "HGNC",
+            "PubChem Compound"
         ]
     else:
         assert isinstance(output_datasource, list), "output_datasource must be a list"
