6618f0bf355b2a0a9a10212059b79ef883efbb05
Javier <javier.millanacosta@maastrichtuniversity.nl>
2025-04-29 14:52:41 +0200

    Tox

--- src/pyBiodatafuse/graph/rdf/nodes/aop.py
+++ src/pyBiodatafuse/graph/rdf/nodes/aop.py
@@ -7,7 +7,7 @@ from rdflib import Graph, Literal, URIRef
 from rdflib.namespace import OWL, RDF, RDFS, XSD
 
 from pyBiodatafuse.constants import CLINICAL_PHASES, MOAS, NODE_TYPES, PREDICATES
-from pyBiodatafuse.graph.rdf.nodes.compound import add_compound_gene_node
+from pyBiodatafuse.graph.rdf.nodes.compound import add_compound_node
 
 
 def add_aop_compound_node(args):
