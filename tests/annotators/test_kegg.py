912cea300eedb0173f36b7525d84f6efb7f887ec
YojanaGadiya <yojanagadiya@gmail.com>
2025-05-20 18:09:42 +0200

    relocate kegg test and fix kegg

---
+++ tests/annotators/test_kegg.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for the KEGG annotator."""
+
+import unittest
+from unittest.mock import Mock, patch
+
+import pandas as pd
+
+from pyBiodatafuse.annotators import kegg
+from pyBiodatafuse.annotators.kegg import check_version_kegg, get_pathways
+from pyBiodatafuse.constants import KEGG_PATHWAY_COL
+
+
+class TestKEGG(unittest.TestCase):
+    """Test the KEGG annotation module."""
+
+    def test_get_pathways(self):
+        """Test the get_pathways function."""
+        kegg.check_version_kegg = Mock(return_value={"source_version": "KEGG RDF 20240315"})
+        kegg.check_endpoint_kegg = Mock(return_value=True)
+
+        input_df = pd.DataFrame(
+            {
+                "identifier": ["ENSMUSG00000067274"],
+                "identifier.source": ["Ensembl"],
+                "target": ["11837"],
+                "target.source": ["NCBI Gene"],
+            }
+        )
+
+        result_df, metadata = get_pathways(input_df)
+
+        expected_pathways = pd.Series(
+            [
+                [
+                    {
+                        "KEGG_pathway_id": "path:mmu03010",
+                        "KEGG_pathway_label": "Ribosome - Mus musculus (house mouse)",
+                        "KEGG_gene_counts": 274,
+                        "KEGG_pathway_compounds": [{"KEGG_id": None}],
+                    },
+                    {
+                        "KEGG_pathway_id": "path:mmu05171",
+                        "KEGG_pathway_label": "Coronavirus disease - COVID-19 - Mus musculus (house mouse)",
+                        "KEGG_gene_counts": 333,
+                        "KEGG_pathway_compounds": [
+                            {"KEGG_id": "C00027"},
+                            {"KEGG_id": "C00046"},
+                            {"KEGG_id": "C00165"},
+                            {"KEGG_id": "C00290"},
+                            {"KEGG_id": "C00533"},
+                            {"KEGG_id": "C00704"},
+                            {"KEGG_id": "C00873"},
+                            {"KEGG_id": "C02135"},
+                            {"KEGG_id": "C15850"},
+                            {"KEGG_id": "C20640"},
+                        ],
+                    },
+                ]
+            ]
+        )
+
+        expected_pathways.name = KEGG_PATHWAY_COL
+
+        pd.testing.assert_series_equal(result_df[KEGG_PATHWAY_COL], expected_pathways)
+        self.assertIsInstance(metadata, dict)
