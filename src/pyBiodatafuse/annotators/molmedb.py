b857a0db48d810c525b2fe0c2ef74c7af0e02606
YojanaGadiya <yojanagadiya@gmail.com>
2025-05-23 11:37:23 +0200

    update molmedb

--- src/pyBiodatafuse/annotators/molmedb.py
+++ src/pyBiodatafuse/annotators/molmedb.py
@@ -13,16 +13,7 @@ from typing import Any, Dict, Tuple
 import pandas as pd
 from SPARQLWrapper import JSON, SPARQLWrapper
 
-from pyBiodatafuse.constants import (
-    MOLMEDB,
-    MOLMEDB_COMPOUND_INPUT_ID,
-    MOLMEDB_COMPOUND_PROTEIN_COL,
-    MOLMEDB_COMPOUND_PROTEIN_OUTPUT_DICT,
-    MOLMEDB_ENDPOINT,
-    MOLMEDB_PROTEIN_COMPOUND_COL,
-    MOLMEDB_PROTEIN_COMPOUND_OUTPUT_DICT,
-    MOLMEDB_PROTEIN_INPUT_ID,
-)
+import pyBiodatafuse.constants as Cons
 from pyBiodatafuse.utils import (
     check_columns_against_constants,
     collapse_data_sources,
@@ -41,7 +32,7 @@ def check_endpoint_molmedb() -> bool:
         <https://identifiers.org/molmedb/MM00040> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?t
         }
         """
-    sparql = SPARQLWrapper(MOLMEDB_ENDPOINT)
+    sparql = SPARQLWrapper(Cons.MOLMEDB_ENDPOINT)
     sparql.setOnlyConneg(True)
     sparql.setQuery(query_string)
     try:
@@ -52,6 +43,9 @@ def check_endpoint_molmedb() -> bool:
 
 
 # TODO - Add metadata function. Currently, no metadata is returned from IDSM servers
+# def check_version_molmedb():
+#     """Check the current version of the MolMeDB database."""
+#     pass
 
 
 def get_gene_compound_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame, dict]:
@@ -64,15 +58,15 @@ def get_gene_compound_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
     api_available = check_endpoint_molmedb()
     if not api_available:
         warnings.warn(
-            f"{MOLMEDB} endpoint is not available. Unable to retrieve data.", stacklevel=2
+            f"{Cons.MOLMEDB} endpoint is not available. Unable to retrieve data.", stacklevel=2
         )
         return pd.DataFrame(), {}
 
     # Record the start time
     start_time = datetime.datetime.now()
 
-    data_df = get_identifier_of_interest(bridgedb_df, MOLMEDB_PROTEIN_INPUT_ID)
-    molmedb_transporter_list = data_df["target"].tolist()
+    data_df = get_identifier_of_interest(bridgedb_df, Cons.MOLMEDB_PROTEIN_INPUT_ID)
+    molmedb_transporter_list = data_df[Cons.TARGET_COL].tolist()
 
     molmedb_transporter_list = list(set(molmedb_transporter_list))
 
@@ -91,7 +85,7 @@ def get_gene_compound_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
     ) as fin:
         sparql_query = fin.read()
 
-    sparql = SPARQLWrapper(MOLMEDB_ENDPOINT)
+    sparql = SPARQLWrapper(Cons.MOLMEDB_ENDPOINT)
     sparql.setReturnFormat(JSON)
     sparql.setOnlyConneg(True)
 
@@ -113,9 +107,9 @@ def get_gene_compound_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
         if df.empty:
             continue
         # Merging the source_pmid values for each unique compound-gene pair
-        cols = [col for col in df.columns.to_list() if col != "source_pmid"]
+        cols = [col for col in df.columns.to_list() if col != Cons.SOURCE_PMID]
 
-        df2 = df.groupby(cols)["source_pmid"].apply(lambda x: ", ".join(x)).reset_index()
+        df2 = df.groupby(cols)[Cons.SOURCE_PMID].apply(lambda x: ", ".join(x)).reset_index()
 
         intermediate_df = pd.concat([intermediate_df, df2], ignore_index=True)  # adds to the time
 
@@ -123,11 +117,11 @@ def get_gene_compound_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
     end_time = datetime.datetime.now()
 
     if intermediate_df.empty:
+        warnings.warn(
+            f"There is no annotation for your input list in {Cons.MOLMEDB}.",
+            stacklevel=2,
+        )
         return intermediate_df, {}
-
-    intermediate_df["drugbank_id"] = intermediate_df["drugbank_id"].apply(
-        lambda x: f"DrugBank:{x}" if not pd.isna(x) else None
-    )
 
     """Metdata details"""
     # Get the current date and time
@@ -137,85 +131,73 @@ def get_gene_compound_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
 
     # Add the datasource, query, query time, and the date to metadata
     molmedb_metadata: Dict[str, Any] = {
-        "datasource": MOLMEDB,
+        "datasource": Cons.MOLMEDB,
         "query": {
             "size": len(molmedb_transporter_list),
-            "input_type": MOLMEDB_PROTEIN_INPUT_ID,
+            "input_type": Cons.MOLMEDB_PROTEIN_INPUT_ID,
             "time": time_elapsed,
             "date": current_date,
-            "url": MOLMEDB_ENDPOINT,
+            "url": Cons.MOLMEDB_ENDPOINT,
         },
     }
 
-    if "transporterID" not in intermediate_df.columns:
-        warnings.warn(
-            f"There is no annotation for your input list in {MOLMEDB}.",
-            stacklevel=2,
-        )
-        return pd.DataFrame(), molmedb_metadata
-
     # Organize the annotation results as an array of dictionaries
     intermediate_df.rename(
-        columns={
-            "transporterID": "target",
-            "pubchem_compound_id": "compound_cid",
-            "label": "compound_name",
-            "SMILES": "smiles",
-            "InChIKey": "inchikey",
-        },
+        columns=Cons.MAP_COMPOUND_COL_NAMES,
         inplace=True,
     )
-    intermediate_df["uniprot_trembl_id"] = intermediate_df["target"]
 
     # Check if all keys in df match the keys in OUTPUT_DICT
     check_columns_against_constants(
         data_df=intermediate_df,
-        output_dict=MOLMEDB_PROTEIN_COMPOUND_OUTPUT_DICT,
-        check_values_in=["molmedb_id", "source_doi", "drugbank_id"],
+        output_dict=Cons.MOLMEDB_PROTEIN_COMPOUND_OUTPUT_DICT,
+        check_values_in=[Cons.MOLMEDB_COMPOUND_DEFAULT_ID],
     )
 
     # Merge the two DataFrames on the target column
     merged_df = collapse_data_sources(
         data_df=data_df,
-        source_namespace=MOLMEDB_PROTEIN_INPUT_ID,
+        source_namespace=Cons.MOLMEDB_PROTEIN_INPUT_ID,
         target_df=intermediate_df,
-        common_cols=["target"],
-        target_specific_cols=list(MOLMEDB_PROTEIN_COMPOUND_OUTPUT_DICT.keys()),
-        col_name=MOLMEDB_PROTEIN_COMPOUND_COL,
+        common_cols=[Cons.TARGET_COL],
+        target_specific_cols=list(Cons.MOLMEDB_PROTEIN_COMPOUND_OUTPUT_DICT.keys()),
+        col_name=Cons.MOLMEDB_PROTEIN_COMPOUND_COL,
     )
 
     # Ensuring all the dictionaries in the MolMeDB_transporter_inhibitor column are same for multiple gene isoforms
     main_df = []
 
-    for source in merged_df["identifier"].unique():
-        mm = merged_df[merged_df["identifier"] == source]
+    for source in merged_df[Cons.IDENTIFIER_COL].unique():
+        mm = merged_df[merged_df[Cons.IDENTIFIER_COL] == source]
         if len(mm) < 2:
             main_df.append(mm)
             continue
 
-        molmedb_output = list(mm[MOLMEDB_PROTEIN_COMPOUND_COL].values)
+        molmedb_output = list(mm[Cons.MOLMEDB_PROTEIN_COMPOUND_COL].values)
         unique_output = get_unique_dicts(molmedb_output)
-        mm[MOLMEDB_PROTEIN_COMPOUND_COL] = ([unique_output]) * len(mm)
+        mm[Cons.MOLMEDB_PROTEIN_COMPOUND_COL] = ([unique_output]) * len(mm)
         main_df.append(mm)
 
     main_df = pd.concat(main_df)
 
     """Update metadata"""
     # Calculate the number of new nodes
-    num_new_nodes = intermediate_df["molmedb_id"].nunique()
+    num_new_nodes = intermediate_df[Cons.MOLMEDB_ID].nunique()
     # Calculate the number of new edges
-    num_new_edges = intermediate_df.drop_duplicates(subset=["target", "molmedb_id"]).shape[0]
+    num_new_edges = intermediate_df.drop_duplicates(
+        subset=[Cons.TARGET_COL, Cons.MOLMEDB_ID]
+    ).shape[0]
 
     # Check the intermediate_df
     if num_new_edges != len(intermediate_df):
         warnings.warn(
-            f"The intermediate_df in {MOLMEDB} annotator should be checked, please create an issue on https://github.com/BioDataFuse/pyBiodatafuse/issues/.",
+            f"The intermediate_df in {Cons.MOLMEDB} annotator should be checked, please create an issue on https://github.com/BioDataFuse/pyBiodatafuse/issues/.",
             stacklevel=2,
         )
 
     # Add the number of new nodes and edges to metadata
-    molmedb_metadata["query"]["number_of_added_nodes"] = num_new_nodes
-    molmedb_metadata["query"]["number_of_added_edges"] = num_new_edges
+    molmedb_metadata[Cons.QUERY][Cons.NUM_NODES] = num_new_nodes
+    molmedb_metadata[Cons.QUERY][Cons.NUM_EDGES] = num_new_edges
 
     return main_df, molmedb_metadata
 
@@ -230,15 +212,15 @@ def get_compound_gene_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
     api_available = check_endpoint_molmedb()
     if not api_available:
         warnings.warn(
-            f"{MOLMEDB} endpoint is not available. Unable to retrieve data.", stacklevel=2
+            f"{Cons.MOLMEDB} endpoint is not available. Unable to retrieve data.", stacklevel=2
         )
         return pd.DataFrame(), {}
 
     # Record the start time
     start_time = datetime.datetime.now()
 
-    data_df = get_identifier_of_interest(bridgedb_df, MOLMEDB_COMPOUND_INPUT_ID)
-    inhibitor_list_str = data_df["target"].tolist()
+    data_df = get_identifier_of_interest(bridgedb_df, Cons.MOLMEDB_COMPOUND_INPUT_ID)
+    inhibitor_list_str = data_df[Cons.TARGET_COL].tolist()
 
     inhibitor_list_str = list(set(inhibitor_list_str))
 
@@ -257,7 +239,7 @@ def get_compound_gene_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
     ) as fin:
         sparql_query = fin.read()
 
-    sparql = SPARQLWrapper(MOLMEDB_ENDPOINT)
+    sparql = SPARQLWrapper(Cons.MOLMEDB_ENDPOINT)
     sparql.setReturnFormat(JSON)
     sparql.setOnlyConneg(True)
 
@@ -271,6 +253,7 @@ def get_compound_gene_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
         sparql.setQuery(sparql_query_template_sub)
 
         res = sparql.queryAndConvert()
+        print(res)
 
         df = pd.DataFrame(res["results"]["bindings"])
 
@@ -279,7 +262,13 @@ def get_compound_gene_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
 
         # Merging the source_pmid values for each unique compound-gene pair
         df2 = (
-            df.groupby(["inhibitorInChIKey", "uniprot_trembl_id", "hgcn_id"])["source_pmid"]
+            df.groupby(
+                [
+                    Cons.MOLMEDB_INHIBITOR_INCHIKEY,
+                    Cons.MOLMEDB_UNIPROT_TREMBL_ID,
+                    Cons.MOLMEDB_HGNC_ID,
+                ]
+            )[Cons.SOURCE_PMID]
             .apply(lambda x: ", ".join(x))
             .reset_index()
         )
@@ -288,6 +277,17 @@ def get_compound_gene_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
 
     # Record the end time
     end_time = datetime.datetime.now()
+
+    if intermediate_df.empty:
+        warnings.warn(
+            f"There is no annotation for your input list in {Cons.MOLMEDB}.",
+            stacklevel=2,
+        )
+        return pd.DataFrame(), {}
+
+    # Organize the annotation results as an array of dictionaries
+    intermediate_df.rename(columns=Cons.MAP_GENE_COL_NAMES, inplace=True)
+
     """Metdata details"""
     # Get the current date and time
     current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -296,67 +296,61 @@ def get_compound_gene_inhibitor(bridgedb_df: pd.DataFrame) -> Tuple[pd.DataFrame
 
     # Add the datasource, query, query time, and the date to metadata
     molmedb_metadata: Dict[str, Any] = {
-        "datasource": MOLMEDB,
+        "datasource": Cons.MOLMEDB,
         "query": {
             "size": len(inhibitor_list_str),
-            "input_type": MOLMEDB_COMPOUND_INPUT_ID,
+            "input_type": Cons.MOLMEDB_COMPOUND_INPUT_ID,
             "time": time_elapsed,
             "date": current_date,
-            "url": MOLMEDB_ENDPOINT,
+            "url": Cons.MOLMEDB_ENDPOINT,
         },
     }
-
-    if "inhibitorInChIKey" not in intermediate_df.columns:
-        warnings.warn(
-            f"There is no annotation for your input list in {MOLMEDB}.",
-            stacklevel=2,
-        )
-        return pd.DataFrame(), molmedb_metadata
-
-    # Organize the annotation results as an array of dictionaries
-    intermediate_df.rename(
-        columns={"inhibitorInChIKey": "target", "hgcn_id": "hgnc_symbol"}, inplace=True
-    )
 
     # Check if all keys in df match the keys in OUTPUT_DICT
     check_columns_against_constants(
         data_df=intermediate_df,
-        output_dict=MOLMEDB_COMPOUND_PROTEIN_OUTPUT_DICT,
-        check_values_in=["UNIPROT_TREMBL_ID"],
+        output_dict=Cons.MOLMEDB_COMPOUND_PROTEIN_OUTPUT_DICT,
+        check_values_in=[Cons.MOLMEDB_UNIPROT_TREMBL_ID],
     )
 
     # Merge the two DataFrames on the target column
     merged_df = collapse_data_sources(
         data_df=data_df,
-        source_namespace=MOLMEDB_COMPOUND_INPUT_ID,
+        source_namespace=Cons.MOLMEDB_COMPOUND_INPUT_ID,
         target_df=intermediate_df,
-        common_cols=["target"],
-        target_specific_cols=list(MOLMEDB_COMPOUND_PROTEIN_OUTPUT_DICT.keys()),
-        col_name=MOLMEDB_COMPOUND_PROTEIN_COL,
+        common_cols=[Cons.TARGET_COL],
+        target_specific_cols=list(Cons.MOLMEDB_COMPOUND_PROTEIN_OUTPUT_DICT.keys()),
+        col_name=Cons.MOLMEDB_COMPOUND_PROTEIN_COL,
     )
 
     """Update metadata"""
     # Calculate the number of new nodes
-    num_new_nodes = intermediate_df["uniprot_trembl_id"].nunique()
+    num_new_nodes = intermediate_df[Cons.MOLMEDB_UNIPROT_TREMBL_ID].nunique()
     # Calculate the number of new edges
-    num_new_edges = intermediate_df.drop_duplicates(subset=["target", "uniprot_trembl_id"]).shape[0]
+    num_new_edges = intermediate_df.drop_duplicates(
+        subset=[Cons.TARGET_COL, Cons.MOLMEDB_UNIPROT_TREMBL_ID]
+    ).shape[0]
 
     # Check the intermediate_df
     if num_new_edges != len(intermediate_df):
         warnings.warn(
-            f"The intermediate_df in {MOLMEDB} annotator should be checked, please create an issue on https://github.com/BioDataFuse/pyBiodatafuse/issues/.",
+            f"The intermediate_df in {Cons.MOLMEDB} annotator should be checked, please create an issue on https://github.com/BioDataFuse/pyBiodatafuse/issues/.",
             stacklevel=2,
         )
 
     # Add the number of new nodes and edges to metadata
-    molmedb_metadata["query"]["number_of_added_nodes"] = num_new_nodes  #
-    molmedb_metadata["query"]["number_of_added_edges"] = num_new_edges  # noqa
+    molmedb_metadata[Cons.QUERY][Cons.NUM_NODES] = num_new_nodes  #
+    molmedb_metadata[Cons.QUERY][Cons.NUM_EDGES] = num_new_edges  # noqa
 
     return merged_df, molmedb_metadata
 
 
 def get_unique_dicts(list_of_list_of_dicts: list) -> list:
-    """Return list of unique dictionaries."""
+    """Return list of unique dictionaries.
+
+    :param list_of_list_of_dicts: list of list of dictionaries
+    :returns: list of unique dictionaries
+    """
     seen = set()
     unique_dicts = []
 
@@ -364,7 +358,7 @@ def get_unique_dicts(list_of_list_of_dicts: list) -> list:
         for d in list_of_dicts:
             # Convert dictionary to frozenset of its items (which is hashable)
             dict_items = frozenset(d.items())
-            if not isinstance(d["inchikey"], str) and isnan(d["inchikey"]):
+            if not isinstance(d[Cons.MOLMEDB_INCHIKEY], str) and isnan(d[Cons.MOLMEDB_INCHIKEY]):
                 empty = d
                 continue
             if dict_items not in seen:
