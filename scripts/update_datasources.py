88198ba3a6aa718c807c008ccd3079bfefa2e04d
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-08-19 23:06:59 +0200

    Make script directly runnable

--- scripts/update_datasources.py
+++ scripts/update_datasources.py
@@ -1,20 +1,27 @@
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [
+#     "bioregistry",
+#     "pandas",
+# ]
+# ///
+#
+
 """
-update_datasources.py
+This script updates the `resources/datasources.csv` file with pattern data fetched from the Bioregistry.
 
-This script updates the `resources/datasources.csv` file with pattern data fetched from the Bioregistry API.
+This script can be run with ``uv``, which automatically creates
+an environment with the appropriate dependencies using:
 
-Usage:
-    python update_datasources.py
+.. code-block:: console
 
-Requirements:
-    - Python 3.9-3.10
-    - pandas
-    - bioregistry
+    $ git clone https://github.com/BioDataFuse/pyBiodatafuse
+    $ cd pyBiodatafuse
+    $ uv run update_datasources.py
 
 Notes:
-    - The Bioregistry API is accessed using the URL template:
-      "https://bioregistry.io/api/registry/{prefix}?format=json".
-    - If a prefix does not have a corresponding pattern in the API, an empty string is used.
+    - Patterns are accesed via the Bioregistry package's :func:`bioregistry.get_pattern`
+    - If a prefix does not have a corresponding pattern in the Bioregistry, an empty string is used.
     - This script is intended for occasional use to update the resource file.
 
 Raises:
@@ -23,29 +30,30 @@ Raises:
 
 import bioregistry
 import pandas as pd
+from pathlib import Path
+
+HERE = Path(__file__).parent.resolve()
+ROOT = HERE.parent.resolve()
+PATH = ROOT.joinpath("src", "pyBiodatafuse", "resources", "datasources.csv")
 
 
 def update_datasources_file():
     """
-    Updates the datasources.csv file with pattern data fetched from the Bioregistry API.
+    Updates the datasources.csv file with pattern data fetched from the Bioregistry.
 
     Note:
-        - The Bioregistry API is accessed via :func:`bioregistry.get_pattern`
-        - If a prefix does not have a corresponding pattern in the API, an empty string is used.
+        - Patterns are accesed via the Bioregistry package's :func:`bioregistry.get_pattern`
+        - If a prefix does not have a corresponding pattern in the Bioregistry, an empty string is used.
 
     Raises:
         FileNotFoundError: If the datasources.csv file is not found.
     """
-
-    # Access the datasources.csv file using importlib.resources
-    csv_path = "resources/datasources.csv"
- 
-    df = pd.read_csv(csv_path)
+    df = pd.read_csv(PATH)
 
     df["pattern"] = df["prefix"].map(bioregistry.get_pattern, na_action="ignore")
 
     # Write the updated data back to the CSV file
-    df.to_csv(csv_path, index=False)
+    df.to_csv(PATH, index=False)
 
 
 if __name__ == "__main__":
