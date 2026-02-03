bd52f3855e35c14abf04448c1cf25b91273a7859
Yojana Gadiya <45199062+YojanaGadiya@users.noreply.github.com>
2026-02-03 13:09:10 +0100

    Minor fixes on patent  (#274)

--- src/pyBiodatafuse/analyzer/utils.py
+++ src/pyBiodatafuse/analyzer/utils.py
@@ -13,16 +13,19 @@ import seaborn as sns
 def plot_pie_chart(
     template_df: pd.DataFrame,
     fig_size: tuple = (10, 10),
+    fig_title: str = "Pie plot",
 ) -> plt:
     """Plot a pie chart.
 
     :param template_df: A dataframe with two columns: "label" and "value"
     :param fig_size: A tuple with the size of the figure
+    :param fig_title: The title of the figure
     :returns: A pie chart
     """
     plt.figure(figsize=fig_size)
     colors = sns.color_palette("pastel")[: len(template_df)]
     plt.pie(template_df["value"], labels=template_df["label"], colors=colors)
+    plt.title(fig_title, size=14)
     return plt.show()
 
 
@@ -31,6 +34,7 @@ def plot_hbarplot_chart(
     x_label: str = "Label",
     y_label: str = "Value",
     fig_size: tuple = (10, 10),
+    fig_title: str = "Bar plot",
 ) -> plt:
     """Plot a bar plot.
 
@@ -38,6 +42,7 @@ def plot_hbarplot_chart(
     :param x_label: The x-axis label
     :param y_label: The y-axis label
     :param fig_size: A tuple with the size of the figure
+    :param fig_title: The title of the figure
     :returns: A bar plot
     """
     plt.figure(figsize=fig_size)
@@ -56,6 +61,7 @@ def plot_hbarplot_chart(
     plt.ylabel(y_label, size=12)
     plt.xticks(fontsize=10)
     plt.yticks(fontsize=10)
+    plt.title(fig_title, size=14)
 
     plt.tight_layout()
 
@@ -68,15 +74,18 @@ def plot_hbarplot_chart(
 def plotly_pie_chart(
     template_df: pd.DataFrame,
     fig_size: tuple = (10, 10),
+    fig_title: str = "Pie plot",
 ) -> px:
     """Plot a pie chart using Plotly.
 
     :param template_df: A dataframe with two columns: "label" and "value"
     :param fig_size: A tuple with the size of the figure
+    :param fig_title: The title of the figure
     :returns: A plotly pie chart
     """
     fig = px.pie(template_df, values="value", names="label", width=fig_size[0], height=fig_size[1])
-    return fig.show()
+    fig.update_layout(title_text=fig_title)
+    return fig
 
 
 def plotly_barplot_chart(
@@ -84,6 +93,7 @@ def plotly_barplot_chart(
     x_label: str = "Label",
     y_label: str = "Value",
     fig_size: tuple = (10, 10),
+    fig_title: str = "Bar plot",
 ) -> px:
     """Plot a bar plot using Plotly.
 
@@ -91,10 +101,12 @@ def plotly_barplot_chart(
     :param x_label: The x-axis label
     :param y_label: The y-axis label
     :param fig_size: A tuple with the size of the figure
+    :param fig_title: The title of the figure
     :returns: A bar plot
     """
     fig = px.bar(template_df, x="value", y="label").update_layout(
         xaxis_title=x_label, yaxis_title=y_label, width=fig_size[0], height=fig_size[1]
     )
+    fig.update_layout(title_text=fig_title)
 
-    return fig.show()
+    return fig
